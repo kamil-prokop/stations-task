d4685586d1d56fd0b0cbad1aaa484b5644231f98
Kamil Prokop <zara.pug.yt@gmail.com>
2023-03-19 23:51:58 +0100

    select and fetchall added

--- stations-task-catalogue/stations-task-done.py
+++ stations-task-catalogue/stations-task-done.py
@@ -34,3 +34,38 @@ connection.commit()
 cursor.execute(sql_clean_measures)
 connection.commit()
 connection.close()
+
+#inserting data from tables
+connection = sqlite3.connect("stations-task.db")
+cursor = connection.cursor()
+
+with open('clean_measure.csv', 'r') as f:
+    for row in f:
+        cursor.execute("INSERT OR IGNORE INTO clean_measures (station, date, precip, tobs) VALUES (?,?,?,?)", row.split(","))
+        connection.commit()
+
+with open('clean_stations.csv', 'r') as f:
+    for row in f:
+        cursor.execute("INSERT OR IGNORE INTO clean_stations (station, latitude, longitude, elevation, name, country, state) VALUES (?,?,?,?,?,?,?)", row.split(","))
+        connection.commit()
+
+connection.close()
+
+connection = sqlite3.connect("stations-task.db")
+cursor = connection.cursor()
+sql_select_m = f"SELECT * FROM clean_measures LIMIT 6"
+sql_select_s = f"SELECT * FROM clean_stations LIMIT 6"
+cursor.execute(sql_select_m)
+results = cursor.fetchall()
+print(results[1:6])
+
+
+'''
+connection = sqlite3.connect("stations-task.db")
+sql_delete_m = f"DELETE FROM clean_measures"
+sql_delete_s = f"DELETE FROM clean_stations"
+cursor = connection.cursor()
+cursor.execute(sql_delete)
+connection.commit()
+connection.close()
+'''
