addbb9a52b1bad97948b2b04bfdcfa7aea4324d0
Kamil Prokop <zara.pug.yt@gmail.com>
2023-03-19 17:30:50 +0100

    error solved concerning creating tables

--- stations-task-catalogue/stations-task-done.py
+++ stations-task-catalogue/stations-task-done.py
@@ -1,29 +1,32 @@
 import sqlite3
 
-#creating 2 databases with names of columns
-connection = sqlite3.connect('stations-task.db')
+#creating 1 database with 2 tables with names of columns
+connection = sqlite3.connect("stations-task.db")
 cursor = connection.cursor()
 
-#first database = clean_stations.csv
+
+#first table = clean_stations.csv
 sql_clean_stations = """
-    CREATE TABLE CleanStations (
+CREATE TABLE IF NOT EXISTS clean_stations (
     station TEXT PRIMARY KEY,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
     elevation FLOAT NOT NULL,
     name TEXT NOT NULL,
     country VARCHAR(2) NOT NULL,
-    state VARCHAR(2) NOT NULL,
-    )"""
+    state VARCHAR(2) NOT NULL
+)
+"""
 
-#second database = clean_measures.csv
+#second table = clean_measures.csv
 sql_clean_measures = """
-    CREATE TABLE CleanMeasures (
+CREATE TABLE IF NOT EXISTS clean_measures (
     station TEXT PRIMARY KEY,
     date TEXT NOT NULL,
     precip FLOAT NOT NULL,
-    tobs INTEGER NOT NULL,
-    )"""
+    tobs INTEGER NOT NULL
+)
+"""
 
 cursor.execute(sql_clean_stations)
 connection.commit()
